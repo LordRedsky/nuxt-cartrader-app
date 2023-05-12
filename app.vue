a90f877edb02ffa9f05edcc892009659379ca8c3
lordRedsky <aminmuhammadrizki@gmail.com>
2023-05-12 14:54:55 +0800

    create home page html done

--- app.vue
+++ app.vue
@@ -1,11 +1,3 @@
 <template>
-  <div>
-    <!-- START NAVBAR -->
-    <header
-      class="sticky top-0 z-50 flex justify-between item-center space-x-1 border-b bg-white p-4 shadow-md"
-    >
-      <a class="text-3xl font-mono" href="">cartrader</a>
-    </header>
-    <!-- END NAVBAR -->
-  </div>
+ <div></div>
 </template>
