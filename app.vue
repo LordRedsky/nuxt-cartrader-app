053afecf66c89aa297ca77b3d15391795c507928
lordRedsky <aminmuhammadrizki@gmail.com>
2023-05-12 16:27:27 +0800

    create car component html template

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
 <template>
- <div></div>
+  <div>
+    <!-- NAVBAR START -->
+    <header
+      class="sticky top-0 z-50 flex justify-between item-center space-x-1 border-b bg-white p-4 shadow-md"
+    >
+      <a class="text-3xl font-mono" href="">cartrader</a>
+    </header>
+    <!-- NAVBAR END -->
+
+   
+  </div>
 </template>
