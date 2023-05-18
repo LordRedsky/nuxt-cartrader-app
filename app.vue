dff408594d19b8ee80fac12ffd70e3f1dd66b550
lordRedsky <aminmuhammadrizki@gmail.com>
2023-05-18 11:17:10 +0800

    create car detail template html

--- app.vue
+++ app.vue
@@ -1,13 +1,6 @@
 <template>
   <div>
-    <!-- NAVBAR START -->
-    <header
-      class="sticky top-0 z-50 flex justify-between item-center space-x-1 border-b bg-white p-4 shadow-md"
-    >
-      <a class="text-3xl font-mono" href="">cartrader</a>
-    </header>
-    <!-- NAVBAR END -->
 
-   
+    
   </div>
 </template>
